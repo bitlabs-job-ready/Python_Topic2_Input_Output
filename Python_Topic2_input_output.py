a98bacffbfbbf04c7b58c856b3b42cf6d4346a3c
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 12:17:02 +0530

    Add files via upload

---
+++ Python_Topic2_input_output.py
@@ -0,0 +1,19 @@
+'''
+Python Input and Output(print()) Functions
+
+Write a Python Program to read Employee Number as Integer , Employee Name As String , Employee Salary as Float & print all these Employee Details.
+'''
+
+
+# Give the  input function to reads employee Name
+
+# Give the input function to reads employee Number
+
+# Give the input function to reads employee Salary
+
+#Give the  output(print()) function to print the employee name
+
+#Give the output(print()) function to print the employee number
+
+#Give the output(print()) function to print the employee Salary
+
